2a84518c5aafee138779b6caa7c484d0bfec09e6
JoshVanL <vleeuwenjoshua@gmail.com>
2018-12-30 17:48:03 +0000

    Adds iface module
    Signed-off-by: JoshVanL <vleeuwenjoshua@gmail.com>

--- modules/iface/iface.go
+++ modules/iface/iface.go
@@ -1,14 +1,15 @@
 package iface
 
 import (
-	//"net"
+	"net"
+	"time"
 
 	"github.com/joshvanl/go-i3status/handler"
 	"github.com/joshvanl/go-i3status/protocol"
 )
 
 const (
-	iface = "wlp58s0"
+	ifaceName = "wlp58s0"
 )
 
 func IFace(block *protocol.Block, h *handler.Handler) {
@@ -18,9 +19,36 @@ func IFace(block *protocol.Block, h *handler.Handler) {
 	h.Must(err)
 
 	for {
-		//ifaces, err := net.Interfaces()
-		//if err != nil {
-		//}
+		iface, err := net.InterfaceByName(ifaceName)
+		h.Must(err)
+
+		addrs, err := iface.Addrs()
+		h.Must(err)
+
+		if len(addrs) == 0 {
+			block.FullText = "down"
+			block.Color = "#ee9999"
+		}
+
+		for _, addr := range addrs {
+			v, ok := addr.(*net.IPNet)
+			if !ok || v.IP.To4() == nil {
+				continue
+			}
+
+			block.FullText = v.IP.String()
+			block.Color = "#aaddaa"
+			break
+		}
+
+		h.Tick()
+
 		<-ch
+
+		// netctl uses a pre down hook so have some buffer
+		// so we don't catch the last up interfaces
+		if block.FullText != "down" {
+			time.Sleep(time.Second * 2)
+		}
 	}
 }
